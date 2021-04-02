2b10c0bcb205c8609fbf62c51bc35c91675d8aca
etreznoski <sanchez.elizabethk@gmail.com>
2021-04-01 17:22:18 -0700

    set up event listener and table filter

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,31 @@
 // from data.js
-var tableData = data;
+const tableData = data;
+//button and form variables
+let button = d3.select("#filter-btn");
+let form = d3.select("#form");
+const tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+//create event handler function
+function filterTable() {
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  let inputElement = d3.select("#datetime"),
+  // Get the value property of the input element
+      inputValue = inputElement.property("value");
+  // Use the form input to filter the data by blood type
+  let filterData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+  data.forEach((tableData) => {
+    let row = tbody.append("tr");
+    Object.values(tableData).forEach(value => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+};
+
+// Create event handlers
+button.on("click", filterTable);
+form.on("submit", filterTable);
