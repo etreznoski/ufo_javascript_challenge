cc46a85cef768eb9d4c6d81fe89a4fe857b71b2d
etreznoski <sanchez.elizabethk@gmail.com>
2021-04-01 17:46:32 -0700

    fixed event listener

--- static/js/app.js
+++ static/js/app.js
@@ -5,9 +5,25 @@ let button = d3.select("#filter-btn");
 let form = d3.select("#form");
 const tbody = d3.select("tbody");
 
+//create function to initialize landing page table
+function init () {
+  data.forEach((tableData) => {
+    let row = tbody.append("tr");
+    Object.values(tableData).forEach(value => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  })
+};
+
+
+
+
 //create event handler function
 function filterTable() {
   d3.event.preventDefault();
+
+  tbody.html("");
 
   // Select the input element and get the raw HTML node
   let inputElement = d3.select("#datetime"),
@@ -16,9 +32,9 @@ function filterTable() {
   // Use the form input to filter the data by blood type
   let filterData = tableData.filter(ufo => ufo.datetime === inputValue);
 
-  data.forEach((tableData) => {
+  filterData.forEach((ufo) => {
     let row = tbody.append("tr");
-    Object.values(tableData).forEach(value => {
+    Object.values(ufo).forEach(value => {
       let cell = row.append("td");
       cell.text(value);
     });
@@ -29,3 +45,7 @@ function filterTable() {
 // Create event handlers
 button.on("click", filterTable);
 form.on("submit", filterTable);
+
+// tbody.html("");
+
+init();
